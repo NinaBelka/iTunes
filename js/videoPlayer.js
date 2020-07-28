f8b49d0654c1cd09ea2585a51b4748bc50ca400e
Nina <mysha76@mail.ru>
2020-07-28 12:13:02 +0300

    Реализация видеоплейера

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,3 +1,73 @@
 export const videoPlayerInit = () => {
-  console.log('Video Init');
+
+  // Получение элементов со страницы DOM
+  const videoPlayer = document.querySelector('.video-player'),
+    videoButtonPlay = document.querySelector('.video-button__play'),
+    videoButtonStop = document.querySelector('.video-button__stop'),
+    videoTimePassed = document.querySelector('.video-time__passed'),
+    videoProgress = document.querySelector('.video-progress'),
+    videoTimeTotal = document.querySelector('.video-time__total');
+
+  // Реализация паузы
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove('fa-pause');
+      videoButtonPlay.classList.add('fa-play');
+    } else {
+      videoButtonPlay.classList.add('fa-pause');
+      videoButtonPlay.classList.remove('fa-play');
+    }
+  };
+
+  const togglePlay = () => {
+    if (videoPlayer.paused) {
+      videoPlayer.play();
+    } else {
+      videoPlayer.pause();
+    }
+  };
+
+  // Реализация стоп-кнопки
+
+  const stopPlay = () => {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+  };
+
+  const addZero = n => n < 10 ? '0' + n : n;
+
+  videoPlayer.addEventListener('click', togglePlay);
+
+  videoButtonPlay.addEventListener('click', togglePlay);
+
+  videoPlayer.addEventListener('play', toggleIcon);
+
+  videoPlayer.addEventListener('pause', toggleIcon);
+
+  videoButtonStop.addEventListener('click', stopPlay);
+
+  // Реализация отображения времени просмотра
+  videoPlayer.addEventListener('timeupdate', () => {
+    const currentTime = videoPlayer.currentTime;
+    const duration = videoPlayer.duration;
+
+    videoProgress.value = (currentTime / duration) * 100;
+
+    let minutePassed = Math.floor(currentTime / 60);
+    let secondsPassed = Math.floor(currentTime % 60);
+
+    let minuteTotal = Math.floor(duration / 60);
+    let secondsTotal = Math.floor(duration % 60);
+
+    videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
+    videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`;
+  });
+
+  // Реализация прогресса воспроизведения
+  videoProgress.addEventListener('change', () => {
+    const duration = videoPlayer.duration;
+    const value = videoProgress.value;
+    videoPlayer.currentTime = (value * duration) / 100;
+  });
+
 };
